852da350e192b5cb3de03c9c3517c32c0299c9f9
BartPlaza <grzebyk44@gmail.com>
2018-10-22 23:42:46 +0200

    plans template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,5 +13,53 @@
                 </div>
             </div>
         </div>
-    </div>
+        <div class="columns is-centered">
+            <div class="column is-8-desktop">
+                <div class="columns planes-wrapper">
+                    <div class="column is-6-desktop">
+                        <div class="card plan-default">
+                            <header class="card-header">
+                                <p class="card-header-title">
+                                    Basic
+                                </p>
+                            </header>
+                            <div class="card-content">
+                                <p class="plan-price"><span>Free</span></p>
+                            </div>
+                            <div class="card-content plan-description">
+                                <div class="plan-feature">One dictionary</div>
+                                <div class="plan-feature">Basic exercises</div>
+                                <div class="plan-feature">Google chrome extension</div>
+                            </div>
+                            <footer class="card-footer">
+                                <a href="#" class="button is-medium is-light is-fullwidth">Choose</a>
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="column is-6-desktop">
+                        <div class="card plan-primary">
+                            <header class="card-header">
+                                <p class="card-header-title">
+                                    Professional
+                                </p>
+                            </header>
+                            <div class="card-content">
+                                <p class="plan-price">$ <span class="has-text-primary">5</span> / Month</p>
+                            </div>
+                            <div class="card-content plan-description">
+                                <div class="plan-feature">One dictionary</div>
+                                <div class="plan-feature">Basic exercises</div>
+                                <div class="plan-feature">Google chrome extension</div>
+                                <div class="plan-feature">Advanced exercises</div>
+                                <div class="plan-feature">No words limit</div>
+                                <div class="plan-feature">Email reminders</div>
+                            </div>
+                            <footer class="card-footer">
+                                    <a href="#" class="button is-medium is-primary is-fullwidth">Choose</a>
+                            </footer>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
